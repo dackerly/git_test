54e577708e4393d3f40a2adc33812ab4accc230c
David Ackerly <dackerly@berkeley.edu>
2015-05-26 11:02:01 -0700

    testing

--- read csv.R
+++ read csv.R
@@ -3,3 +3,8 @@ library(RCurl)
 x <- getURL('https://raw.githubusercontent.com/ackerlylab/PPWoodyVeg/master/Data/SoilMoisture2013.csv')
 y <- read.csv(text = x)
 head(y)
+
+
+x <- getURL('https://raw.githubusercontent.com/dackerly/PepperwoodVegPlots/master/2015/Hydro2015/Data/OriginalCSV/SoilMoisture2015.csv')
+y=read.csv(text=x)
+head(y)
